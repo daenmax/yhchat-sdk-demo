4e2d80774cd9ffac44c005c94e8e63d90cb17ff0
DaenMax <1330166565@qq.com>
2023-06-13 22:03:33 +0800

    完成指令消息事件插件测试

---
+++ src/main/java/cn/daenx/yhchatDemo/MessageInstructionDemo.java
@@ -0,0 +1,49 @@
+package cn.daenx.yhchatDemo;
+
+import cn.daenx.yhchatsdk.common.constant.ChatTypeConstant;
+import cn.daenx.yhchatsdk.common.constant.ContentTypeConstant;
+import cn.daenx.yhchatsdk.framework.eventInterface.EventMessageReceiveInstruction;
+import cn.daenx.yhchatsdk.framework.vo.EventMsgVo;
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+/**
+ * 指令消息事件插件
+ */
+@Slf4j
+@Service
+@Order(1)//相同事件插件中的优先级，越小越优先
+public class MessageInstructionDemo implements EventMessageReceiveInstruction {
+    @Override
+    public Integer handle(EventMsgVo eventMsgVo) {
+        String s = JSONUtil.toJsonStr(eventMsgVo);
+        System.out.println(s);
+        String chatType = eventMsgVo.getEvent().getMessage().getChatType();
+        String chatId = eventMsgVo.getEvent().getChat().getChatId();
+        String senderNickname = eventMsgVo.getEvent().getSender().getSenderNickname();
+        String senderId = eventMsgVo.getEvent().getSender().getSenderId();
+        String contentType = eventMsgVo.getEvent().getMessage().getContentType();
+        Integer instructionId = eventMsgVo.getEvent().getMessage().getInstructionId();
+        String instructionName = eventMsgVo.getEvent().getMessage().getInstructionName();
+        String msg = null;
+        if (ContentTypeConstant.TEXT.equals(contentType)) {
+            msg = eventMsgVo.getEvent().getMessage().getContent().getText();
+        } else if (ContentTypeConstant.FORM.equals(contentType)) {
+            HashMap<String, HashMap<String, Object>> formJson = eventMsgVo.getEvent().getMessage().getContent().getFormJson();
+            System.out.println(formJson);
+            msg = JSONUtil.toJsonStr(formJson);
+        }
+        if (ChatTypeConstant.GROUP.equals(chatType)) {
+            log.info("【群聊指令消息】群号[{}]，用户[{}]（{}）触发了指令[{}]({})，附带内容为：{}", chatId, senderId, senderNickname, instructionId, instructionName, msg);
+        } else if (ChatTypeConstant.BOT.equals(chatType)) {
+            log.info("【私聊指令消息】用户[{}]（{}）触发了指令[{}]({})，附带内容为：{}", senderId, senderNickname, instructionId, instructionName, msg);
+        }
+        //返回-1则不再投递后面的同事件插件
+        //返回0则继续投递给后面的同事件插件处理
+        return 0;
+    }
+}
---
+++ src/main/java/cn/daenx/yhchatDemo/MessageNormalDemo.java
@@ -0,0 +1,45 @@
+package cn.daenx.yhchatDemo;
+
+import cn.daenx.yhchatsdk.common.constant.ChatTypeConstant;
+import cn.daenx.yhchatsdk.common.constant.ContentTypeConstant;
+import cn.daenx.yhchatsdk.framework.eventInterface.EventMessageReceiveNormal;
+import cn.daenx.yhchatsdk.framework.vo.EventMsgVo;
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Service;
+
+/**
+ * 普通消息事件插件
+ */
+@Slf4j
+@Service
+@Order(1)//相同事件插件中的优先级，越小越优先
+public class MessageNormalDemo implements EventMessageReceiveNormal {
+    @Override
+    public Integer handle(EventMsgVo eventMsgVo) {
+        String s = JSONUtil.toJsonStr(eventMsgVo);
+        System.out.println(s);
+        String chatType = eventMsgVo.getEvent().getMessage().getChatType();
+        String chatId = eventMsgVo.getEvent().getChat().getChatId();
+        String senderNickname = eventMsgVo.getEvent().getSender().getSenderNickname();
+        String senderId = eventMsgVo.getEvent().getSender().getSenderId();
+        String contentType = eventMsgVo.getEvent().getMessage().getContentType();
+        String msg = null;
+        if (ContentTypeConstant.TEXT.equals(contentType) || ContentTypeConstant.MARKDOWN.equals(contentType)) {
+            msg = eventMsgVo.getEvent().getMessage().getContent().getText();
+        } else if (ContentTypeConstant.IMAGE.equals(contentType)) {
+            msg = eventMsgVo.getEvent().getMessage().getContent().getImageUrl() + " | " + eventMsgVo.getEvent().getMessage().getContent().getImageName() + " | " + eventMsgVo.getEvent().getMessage().getContent().getEtag();
+        } else if (ContentTypeConstant.FILE.equals(contentType)) {
+            msg = eventMsgVo.getEvent().getMessage().getContent().getFileUrl() + " | " + eventMsgVo.getEvent().getMessage().getContent().getFileName() + " | " + eventMsgVo.getEvent().getMessage().getContent().getFileSize() + " | " + eventMsgVo.getEvent().getMessage().getContent().getEtag();
+        }
+        if (ChatTypeConstant.GROUP.equals(chatType)) {
+            log.info("【群聊普通消息】群号[{}]，用户[{}]（{}）发的[{}]消息：{}", chatId, senderId, senderNickname, contentType, msg);
+        } else if (ChatTypeConstant.BOT.equals(chatType)) {
+            log.info("【私聊普通消息】用户[{}]（{}）发的[{}]消息：{}", senderId, senderNickname, contentType, msg);
+        }
+        //返回-1则不再投递后面的相同事件插件
+        //返回0则继续投递给后面的相同事件插件处理
+        return 0;
+    }
+}
