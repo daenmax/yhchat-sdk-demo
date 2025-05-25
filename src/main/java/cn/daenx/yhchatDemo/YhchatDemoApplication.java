95f873b7102979210bafafdc259f6ad9a4bcfc91
DaenMax <1330166565@qq.com>
2025-05-25 16:46:35 +0800

    1.增加了更多接口调用示例 2.同步官网发送消息接口，发送图片/视频/文件需要先调用上传接口 3.升级hutool版本5.8.32到5.8.38

--- src/main/java/cn/daenx/yhchatDemo/YhchatDemoApplication.java
+++ src/main/java/cn/daenx/yhchatDemo/YhchatDemoApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.util.Arrays;
 
 /**
@@ -27,6 +29,24 @@ public class YhchatDemoApplication {
         SpringApplication.run(YhchatDemoApplication.class, args);
         //发送消息
 //        sendMsgDemo();
+
+        //流式发送消息
+//        sendMsgStreamDemo();
+
+        //发送图片消息
+//        sendImageDemo();
+
+        //发送视频消息
+//        sendVideoDemo();
+
+        //发送文件消息
+//        sendFileDemo();
+
+        //发送HTML消息
+//        sendHtmlDemo();
+
+        //消息列表
+//        messageNormalDemo();
 
         //批量发送消息
 //        sendMsgBatchDemo();
@@ -55,6 +75,120 @@ public class YhchatDemoApplication {
                 .addButton("上报", ButtonActionTypeConstant.REPORT, null, "上报成功啦");
         ApiSendMsgRetV1 apiSendRetV1 = ApiUtil.sendMsg(reqV1);
     }
+
+
+    /**
+     * 流式发送消息示例
+     */
+    public static void sendMsgStreamDemo() {
+        try {
+            PipedInputStream pipedInputStream = new PipedInputStream();
+            PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream);
+
+            // 异步 开个线程模拟一下
+            new Thread(() -> {
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        String data = "Message " + (i + 1) + "\n";
+                        pipedOutputStream.write(data.getBytes("UTF-8"));
+                        pipedOutputStream.flush();
+                        // 模拟延迟
+                        Thread.sleep(1000);
+                    }
+                    pipedOutputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            ApiSendMsgStreamReqV1 reqV1 = new ApiSendMsgStreamReqV1()
+                    .Text(RecvTypeConstant.USER, "4137637", pipedInputStream);
+            ApiSendMsgStreamRetV1 apiSendMsgStreamRetV1 = ApiUtil.sendMsgStream(reqV1);
+            System.out.println(apiSendMsgStreamRetV1);
+
+            pipedInputStream.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 发送图片示例
+     */
+    public static void sendImageDemo() {
+        ApiUploadReqV1 reqV1Upload = new ApiUploadReqV1()
+                .Image("C:\\Users\\13301\\Desktop\\111.png");
+        ApiUploadRetV1 uploadRetV1 = ApiUtil.upload(reqV1Upload);
+        if (uploadRetV1.getCode() != 1) {
+            System.out.println("上传图片失败：" + uploadRetV1.getCode() + ":" + uploadRetV1.getMsg());
+            return;
+        }
+        System.out.println(uploadRetV1);
+        ApiSendMsgReqV1 reqV1 = new ApiSendMsgReqV1()
+                .Image(RecvTypeConstant.USER, "4137637", uploadRetV1.getData().getImageKey());
+        ApiSendMsgRetV1 apiSendRetV1 = ApiUtil.sendMsg(reqV1);
+        System.out.println(apiSendRetV1);
+    }
+
+
+    /**
+     * 发送视频示例
+     */
+    public static void sendVideoDemo() {
+        ApiUploadReqV1 reqV1Upload = new ApiUploadReqV1()
+                .Video("C:\\Users\\13301\\Desktop\\222.mp4");
+        ApiUploadRetV1 uploadRetV1 = ApiUtil.upload(reqV1Upload);
+        if (uploadRetV1.getCode() != 1) {
+            System.out.println("上传视频失败：" + uploadRetV1.getCode() + ":" + uploadRetV1.getMsg());
+            return;
+        }
+        System.out.println(uploadRetV1);
+        ApiSendMsgReqV1 reqV1 = new ApiSendMsgReqV1()
+                .Video(RecvTypeConstant.USER, "4137637", uploadRetV1.getData().getVideoKey());
+        ApiSendMsgRetV1 apiSendRetV1 = ApiUtil.sendMsg(reqV1);
+        System.out.println(apiSendRetV1);
+    }
+
+
+    /**
+     * 发送文件示例
+     */
+    public static void sendFileDemo() {
+        ApiUploadReqV1 reqV1Upload = new ApiUploadReqV1()
+                .File("C:\\Users\\13301\\Desktop\\333.txt");
+        ApiUploadRetV1 uploadRetV1 = ApiUtil.upload(reqV1Upload);
+        if (uploadRetV1.getCode() != 1) {
+            System.out.println("上传文件失败：" + uploadRetV1.getCode() + ":" + uploadRetV1.getMsg());
+            return;
+        }
+        System.out.println(uploadRetV1);
+        ApiSendMsgReqV1 reqV1 = new ApiSendMsgReqV1()
+                .File(RecvTypeConstant.USER, "4137637", uploadRetV1.getData().getFileKey());
+        ApiSendMsgRetV1 apiSendRetV1 = ApiUtil.sendMsg(reqV1);
+        System.out.println(apiSendRetV1);
+    }
+
+
+    /**
+     * 发送HTML示例
+     */
+    public static void sendHtmlDemo() {
+        ApiSendMsgReqV1 reqV1 = new ApiSendMsgReqV1()
+                .Html(RecvTypeConstant.GROUP, "956034802", "<del>删除线</del>");
+        ApiSendMsgRetV1 apiSendRetV1 = ApiUtil.sendMsg(reqV1);
+        System.out.println(apiSendRetV1);
+    }
+
+
+    /**
+     * 发送HTML示例
+     */
+    public static void messageNormalDemo() {
+        ApiMessagesReqV1 reqV1 = new ApiMessagesReqV1(RecvTypeConstant.GROUP, "956034802", null, 20, null);
+        ApiMessagesRetV1 apiMessagesRetV1 = ApiUtil.messages(reqV1);
+        System.out.println(apiMessagesRetV1);
+    }
+
 
     /**
      * 批量发送消息示例
